ec793fffcd71d4f0d22917a242cf39634d5acb73
Jinglei Dong <1816750728@qq.com>
2023-06-28 19:52:18 +0800

    将切换按钮与点名按钮使用类封装，使 main 函数更加简洁。 将所有读取操作转移至 xlsx_io 模块。

--- xlsx_io.py
+++ xlsx_io.py
@@ -79,6 +79,40 @@ def name_load_pkl(pkl_file):
         return data
     except FileNotFoundError:
         return -1
-        
+
+def get_config(yml_name="config.yml", json_name="config.json"):
+    try:
+        import yaml
+    except ImportError:
+        print("未找到 yaml 模块，从 json 文件中读取配置文件")
+        import json
+        with open(json_name, 'r', encoding="utf-8") as f:
+            config = json.loads(f.read())
+    else:
+        with open(yml_name, 'r', encoding="utf-8") as f:
+            config = yaml.load(f.read(), Loader=yaml.CLoader)
+    return config
+
+def get_data(config):
+    if("meta" not in config or "names" not in config):
+        data = name_load_pkl(config["pkldir"])
+    else:
+        data = decode(config["meta"], config["names"])
+    if(data == -1):
+        print("元数据未找到，从二进制文件中读取")
+        data = name_load_pkl(config["pkldir"])
+    elif(data == -2):
+        print("名单未找到，从二进制文件中读取")
+        data = name_load_pkl(config["pkldir"])
+    elif(data == -3):
+        print("元数据与名单不匹配，请检查工作表名是否一致")
+        exit()
+    elif(data == 0):
+        print("未找到openpyxl模块，从二进制文件中读取")
+        data = name_load_pkl(config["pkldir"])
+    else:
+        name_dump_pkl(data, config["pkldir"])
+    return data
+
 if __name__ == '__main__':
     print(decode("./config/meta.xlsx", "./config/names.xlsx"))
